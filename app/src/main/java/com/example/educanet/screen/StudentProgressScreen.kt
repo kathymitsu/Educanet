5ec1fa7d24b7adc9801d7accefee32b71295b6bf
dsim1145 <dsim.c2000@gmail.com>
2025-12-16 21:37:13 -0300

    Update StudentProgressScreen.kt

--- app/src/main/java/com/example/educanet/screen/StudentProgressScreen.kt
+++ app/src/main/java/com/example/educanet/screen/StudentProgressScreen.kt
@@ -1,5 +1,5 @@
 package com.example.educanet.screen
-
+import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
